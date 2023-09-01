7d71c786adc99b8578ebaede34f841fab7a9f51d
Sankhadip Das <sankhadip10.das@gmail.com>
2023-09-01 23:52:50 +0530

    Hashig 1 commit added

--- src/DataStructure_Python/Hashing1/commonElements.py
+++ src/DataStructure_Python/Hashing1/commonElements.py
@@ -78,3 +78,34 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# Time and Space Complexity
+# TC:-O(n+m)
+# SC:-O(n + min(n, m))
+
+
+
+# The time and space complexity of this solution can be analyzed as follows:
+#
+# Time Complexity:
+#
+# Building the hashmap for array A: The loop runs for each element in array A, which has n elements. The dictionary operations inside the loop (hashmap.get() and hashmap[A[i]]) take constant time on average. Therefore, the time complexity of building the hashmap for array A is O(n).
+#
+# Finding common elements in array B: Similar to the previous loop, this loop runs for each element in array B, which has m elements. The dictionary operations inside this loop also take constant time. Therefore, the time complexity of finding common elements in array B is O(m).
+#
+# Combining both steps, the overall time complexity of the solution is O(n + m), where n is the size of array A and m is the size of array B.
+#
+# Space Complexity:
+#
+# hashmap: The space required to store the dictionary hashmap depends on the number of distinct elements in array A. In the worst case, all elements are distinct, and the dictionary would store n key-value pairs. Therefore, the space complexity for the hashmap is O(n).
+#
+# result list: The space required to store the common elements in the result list is proportional to the number of common elements between arrays A and B. In the worst case, where all elements are common, the list would store min(n, m) elements. Therefore, the space complexity for the result list is O(min(n, m)).
+#
+# Combining both factors, the overall space complexity of the solution is O(n + min(n, m)), where n is the size of array A and m is the size of array B.
+
+
+
+
+
+
+
--- src/DataStructure_Python/ess.py
+++ src/DataStructure_Python/ess.py
@@ -1,4 +1,2 @@
-freq_A={1:'33'}
+freq_A={"1":'33'}
 
-for x in A:
-    freq_A[x]=freq_A.get(x,0+1)
