a079616d8f3dfe898593a262078926dec08ea7da
Sankhadip Das <sankhadip10.das@gmail.com>
2023-09-26 23:51:33 +0530

    fff

--- src/DataStructure_Python/ess.py
+++ src/DataStructure_Python/ess.py
@@ -144,31 +144,34 @@
 #     freq[char] = freq.get(char, 0) + 1
 #     print(freq)
 
-class Solution:
-    # @param A : string
-    # @return a strings
-    def solve(self, A):
-        minchar = 'z'
-        idx = 1000000000
-        for i in range(len(A) - 1):
-            if A[i] < minchar:
-                minchar = A[i]
-                idx = i
-
-        minchar2 = 'z'
-        for i in range(idx + 1, len(A)):
-            if (A[i] < minchar2):
-                minchar2 = A[i]
-
-        ans = minchar + minchar2
-        return ans
-
-def main():
-    solution =Solution()
-    A="badc"
-    res=solution.solve(A)
-    print(res)
-
-if __name__ == "__main__":
-    main()
-
+# class Solution:
+#     # @param A : string
+#     # @return a strings
+#     def solve(self, A):
+#         minchar = 'z'
+#         idx = 1000000000
+#         for i in range(len(A) - 1):
+#             if A[i] < minchar:
+#                 minchar = A[i]
+#                 idx = i
+#
+#         minchar2 = 'z'
+#         for i in range(idx + 1, len(A)):
+#             if (A[i] < minchar2):
+#                 minchar2 = A[i]
+#
+#         ans = minchar + minchar2
+#         return ans
+#
+# def main():
+#     solution =Solution()
+#     A="badc"
+#     res=solution.solve(A)
+#     print(res)
+#
+# if __name__ == "__main__":
+#     main()
+#
+# A=[1,2,3,4,5]
+# for j, num in enumerate(A):
+#     print(j,num)
